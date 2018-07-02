6c4bcdd80aaf2900bd25fa5f29e63fb09c5e9993
Ryusei Tabata <ryuseitabata@ryuseinoimac.local>
2018-07-02 15:42:19 +0900

    Initial Commit

---
+++ PreventVideoiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PreventVideoiOS
+//
+//  Created by Ryusei Tabata on 2018/07/02.
+//  Copyright © 2018年 Ryusei Tabata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
